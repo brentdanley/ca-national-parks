49faf0715729148d9dd3b068b2b35203a9da4661
Brent Danley <brentdanley@gmail.com>
2021-09-05 15:10:50 -0400

    Styles activity list

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import styles from '../styles/header.module.scss'
+import CA_flag from '../images/ca-flag.jpg'
 
 const Header = () => {
 
     return (
         <header className={styles.header}>
-            <p>Brent</p>
+            <div className={styles.imageWrapper}>
+                <Image
+                    src={ CA_flag }
+                    alt='California State Flag'
+                />
+            </div>
+            <p className={styles.siteName}>National Parks of California</p>
         </header>
     )
 }
--- components/SingleCard.js
+++ components/SingleCard.js
@@ -1,12 +1,45 @@
 import styles from '../styles/singlecard.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkedAlt, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 
-const SingleCard = ({ park }) => (
-    <article className={styles.card} style={{backgroundImage: `url(${park.images[0].url})`}}>
-        <h3 className={styles.parkName}>{park.name}</h3>
-        <div className={styles.contentWrapper}>
-            <p className={styles.description}>{park.description}</p>
-        </div>
-    </article>
-)
+const SingleCard = ({ park }) => {
+
+    const formatLatitude = lat => {
+        const latNumber = Number(lat).toFixed(3)
+
+        return (
+            `${Math.abs(latNumber)} ${latNumber >= 0 ? 'N' : 'S'}`
+        )
+    }
+
+    const formatLongitude = long => {
+        const longNumber = Number(long).toFixed(3)
+
+        return (
+            `${Math.abs(longNumber)} ${longNumber >= 0 ? 'E' : 'W'}`
+        )
+    }
+
+    const activities = []
+    for (var i = 0; i < park.activities.length && i < 3; i++) {
+        activities.push(park.activities[i].name)
+    }
+    console.log(activities)
+
+    return (
+        <article className={styles.card} style={{backgroundImage: `url(${park.images[0].url})`}}>
+            <h3 className={styles.parkName}>{park.name}</h3>
+            <div className={styles.contentWrapper}>
+                <p className={styles.description}>{park.description}</p>
+                <p className={styles.coords}><FontAwesomeIcon icon={faMapMarkedAlt} /> {formatLatitude(park.latitude)} {formatLongitude(park.longitude)}</p>
+                <div className={styles.activities}>
+                    <h5 className={styles.title}>Activities</h5>
+                    <p className={styles.activityList}>{activities.join(', ')}</p>
+                </div>
+                <a className={styles.npsLink} href={park.url} title={`Read more about ${park.name} on NPS.gov`}>Read more at NPS.gov <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+            </div>
+        </article>
+    )
+}
 
 export default SingleCard
